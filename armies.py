768896d3130abdb6bd2748901d59eee2f5198464
BuildTools <unconfigured@null.spigotmc.org>
2023-06-01 21:56:33 -0700

    updating with further oop study

--- armies.py
+++ armies.py
@@ -1,113 +1,135 @@
+from abc import ABC, abstractmethod
 import random
 from names import names
 
+
 class Army:
-  def __init__(self, color, swordsmen_count, archer_count):
-    self.name = f'The {color} Army'
-    self.color = color
-    self.units = []
-    self.defeated = False
-    self.kills = 0
-    for s in range(swordsmen_count):
-      self.units.append(Swordsman(color))
-    for a in range(archer_count):
-      self.units.append(Archer(color))
+    def __init__(self, color, swordsmen_count, archer_count):
+        self.name = f'The {color} Army'
+        self.color = color
+        self.units = []
+        self.defeated = False
+        self.kills = 0
+        for s in range(swordsmen_count):
+            self.units.append(Swordsman(color))
+        for a in range(archer_count):
+            self.units.append(Archer(color))
 
-class Unit:
-  def __init__(self, color):
-    name = random.choice(names)
-    names.remove(name)
-    self.name = name 
-    self.color = color
-    self.health = 100
-    self.alive = True
-    self.currentAttacker = 'none'
-    self.kills = 0
 
-  def attack(self):
-    attack = {
-      "hits": False,
-      "hit_type": 'misses',
-      "damage": 0, 
-    }
-    attempt = random.random()
-    damage = random.randrange(self.min_damage, self.max_damage)
-    if self.min_hit_chance > attempt < self.crit_hit_chance:
-      attack['hits'] = True
-      attack['damage'] = damage
-      attack['outcome_text'] = f'gets a body hit for {damage} damage.'
-    elif attempt > self.crit_hit_chance:
-      damage *= self.crit_multiplyer
-      attack['hits'] = True
-      attack['damage'] = damage
-      attack['outcome_text'] = f'gets a headshot for {damage} damage.'
-    return attack
+class Unit(ABC):
+    def __init__(self, color):
+        name = random.choice(names)
+        names.remove(name)
+        self.name = name
+        self.color = color
+        self.__health = 100
+        self.__alive = True
+        self.currentAttacker = 'none'
+        self.kills = 0
+
+    @abstractmethod
+    def select_attack_target(self):
+        pass
+
+    def attack(self):
+        attack = {
+            "hits": False,
+            "hit_type": 'misses',
+            "damage": 0,
+        }
+        attempt = random.random()
+        damage = random.randrange(self.min_damage, self.max_damage)
+        if self.min_hit_chance > attempt < self.crit_hit_chance:
+            attack['hits'] = True
+            attack['damage'] = damage
+            attack['outcome_text'] = f'gets a body hit for {damage} damage.'
+        elif attempt > self.crit_hit_chance:
+            damage *= self.crit_multiplyer
+            attack['hits'] = True
+            attack['damage'] = damage
+            attack['outcome_text'] = f'gets a headshot for {damage} damage.'
+        return attack
+
+    def get_health(self):
+        return self.__health
+
+    def take_damage(self, damage):
+        if damage < self.__health:
+            self.__health -= damage
+        else:
+            self.__health = 0
+            self.__alive = False
+
+    def is_alive(self):
+        return self.__alive
+
 
 class Swordsman(Unit):
-  def __init__(self, color):
-    super().__init__(color)
-    self.type = 'Swordsman'
-    self.full_name = f'{self.name} The {self.color} {self.type}'
-    self.attack_verb = 'swings'
-    self.armour = 40
-    self.min_hit_chance = .2
-    self.crit_hit_chance = .8
-    self.min_damage = 30
-    self.max_damage = 60
-    self.crit_multiplyer = 2
-    
-  def select_attack_target(self):
-    def find_other_army(armies):
-      selected_army = random.choice(armies)
-      if selected_army.color != self.color and len(selected_army.units) > 0:
-        unit_is_swordsman = []
-        unit_not_swordsman = []
-        # if there are still swordsmen then must attack swordsmen  
-        for unit in selected_army.units:
-          if isinstance(unit, Swordsman):
-            unit_is_swordsman.append(unit)
-          else: 
-            unit_not_swordsman.append(unit)
-        if len(unit_is_swordsman):
-          selected_unit = random.choice(unit_is_swordsman)
-        else:
-          selected_unit = random.choice(unit_not_swordsman)
-        return selected_unit
-      else:
+    def __init__(self, color):
+        super().__init__(color)
+        self.type = 'Swordsman'
+        self.full_name = f'{self.name} The {self.color} {self.type}'
+        self.attack_verb = 'swings'
+        self.armour = 40
+        self.min_hit_chance = .2
+        self.crit_hit_chance = .8
+        self.min_damage = 30
+        self.max_damage = 60
+        self.crit_multiplyer = 2
+
+    def select_attack_target(self):
+        def find_other_army(armies):
+            selected_army = random.choice(armies)
+            if selected_army.color != self.color and len(selected_army.units) > 0:
+                unit_is_swordsman = []
+                unit_not_swordsman = []
+                # if there are still swordsmen then must attack swordsmen
+                for unit in selected_army.units:
+                    if isinstance(unit, Swordsman):
+                        unit_is_swordsman.append(unit)
+                    else:
+                        unit_not_swordsman.append(unit)
+                if len(unit_is_swordsman):
+                    selected_unit = random.choice(unit_is_swordsman)
+                else:
+                    selected_unit = random.choice(unit_not_swordsman)
+                return selected_unit
+            else:
+                selected_unit = find_other_army(armies)
+                return selected_unit
         selected_unit = find_other_army(armies)
         return selected_unit
-    selected_unit = find_other_army(armies)
-    return selected_unit
+
 
 class Archer(Unit):
-  def __init__(self, color):
-    super().__init__(color)
-    self.type = 'Archer'
-    self.full_name = f'{self.name} The {self.color} {self.type}'
-    self.attack_verb = 'fires'
-    self.armour = 10
-    self.arrow_count = 10
-    self.min_hit_chance = .4
-    self.crit_hit_chance = .9
-    self.min_damage = 50
-    self.max_damage = 100
-    self.crit_multiplyer = 3
+    def __init__(self, color):
+        super().__init__(color)
+        self.type = 'Archer'
+        self.full_name = f'{self.name} The {self.color} {self.type}'
+        self.attack_verb = 'fires'
+        self.armour = 10
+        self.arrow_count = 10
+        self.min_hit_chance = .4
+        self.crit_hit_chance = .9
+        self.min_damage = 50
+        self.max_damage = 100
+        self.crit_multiplyer = 3
 
-  def select_attack_target(self):
-    def find_other_army(armies):
-      selected_army = random.choice(armies)
-      if selected_army.color != self.color and len(selected_army.units) > 0:
-        selected_unit = random.choice(selected_army.units)
-        return selected_unit
-      else:
+    def select_attack_target(self):
+        def find_other_army(armies):
+            selected_army = random.choice(armies)
+            if selected_army.color != self.color and len(selected_army.units) > 0:
+                selected_unit = random.choice(selected_army.units)
+                return selected_unit
+            else:
+                selected_unit = find_other_army(armies)
+                return selected_unit
         selected_unit = find_other_army(armies)
         return selected_unit
-    selected_unit = find_other_army(armies)
-    return selected_unit
 
 
 armies = [
-  Army('Red', swordsmen_count=2, archer_count=1),
-  Army('Blue', swordsmen_count=2, archer_count=1),
-  # Army('Green', swordsmen_count=2, archer_count=1),
+    Army('Red', swordsmen_count=2, archer_count=1),
+    Army('Blue', swordsmen_count=2, archer_count=1),
+    # Army('Green', swordsmen_count=2, archer_count=1),
 ]
--- main.py
+++ main.py
@@ -1,49 +1,52 @@
 from armies import armies
 
 # full attack turn
+
+
 def attackTurn():
-  # everyone attacks
-  for army in armies:
-    # print(f'------- \n {army.color} army attacks! \n-------')
-    for attacker in army.units:
-      # input() # to observe turn by turn
-      # print(attacker.__dict__)
-      target = attacker.select_attack_target()
+    # everyone attacks
+    for army in armies:
+        # print(f'------- \n {army.color} army attacks! \n-------')
+        for attacker in army.units:
+            input()  # to observe turn by turn
+            # print(attacker.__dict__)
+            target = attacker.select_attack_target()
 
-      attack = attacker.attack()
-      if attack['hits']: 
-        attack_outcome_text = attack['outcome_text'] 
-      else:
-        attack_outcome_text = 'misses.'
+            attack = attacker.attack()
+            if attack['hits']:
+                attack_outcome_text = attack['outcome_text']
+            else:
+                attack_outcome_text = 'misses.'
 
-      print(f'{attacker.full_name}({attacker.health}) {attacker.attack_verb} at {target.full_name}({target.health}) and {attack_outcome_text}')
+            print(f'{attacker.full_name}({attacker.get_health()}) {attacker.attack_verb} at {target.full_name}({target.get_health()}) and {attack_outcome_text}')
 
-      if attack['hits']: 
-        target.health -= attack['damage']
-        if target.health < 1:
-          target.alive = False
+            if attack['hits']:
+                target.take_damage(attack['damage'])
 
-  # post attack status
-  for army in armies:
-    # print(f'------- \n {army.color} army post-attack status \n-------')
-    for unit in army.units:
-      # print(unit.name, 'The', unit.color, '-', unit.health, unit.alive, 'current attacker: ', unit.currentAttacker)
-      if unit.health < 1:
-        army.units.remove(unit)
-  check_next_turn()
+    # post attack status
+    for army in armies:
+        # print(f'------- \n {army.color} army post-attack status \n-------')
+        for unit in army.units:
+            # print(unit.name, 'The', unit.color, '-', unit.health, unit.alive, 'current attacker: ', unit.currentAttacker)
+            if not unit.is_alive():
+                army.units.remove(unit)
+    check_next_turn()
 
 # check if there should be another turn
+
+
 def check_next_turn():
-  surviving_army = []
-  for army in armies:
-    # print(f'-------- \n{army.color}Army has {len(army.units)} units left')
-    if len(army.units) > 0:
-      surviving_army.append(army)
-  if len(surviving_army) > 1: 
-    attackTurn()
-  elif len(surviving_army) == 1:
-    print(surviving_army[0].name, 'wins!')
-  else:
-    print("Everyone is dead, so let's call it a draw...")
+    surviving_army = []
+    for army in armies:
+        # print(f'-------- \n{army.color}Army has {len(army.units)} units left')
+        if len(army.units) > 0:
+            surviving_army.append(army)
+    if len(surviving_army) > 1:
+        attackTurn()
+    elif len(surviving_army) == 1:
+        print(surviving_army[0].name, 'wins!')
+    else:
+        print("Everyone is dead, so let's call it a draw...")
+
 
 check_next_turn()
